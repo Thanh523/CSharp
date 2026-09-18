1ff0088ebdbcc9f06383d5794d322e74c8f86c7c
Thanh523 <hauthanhnguyenxuan2006@gmail.com>
2026-09-18 08:04:33 +0700

    TH01/TUAN02/Cap nhat bai con thieu
    TH01/TUAN02/Cap nhat bai con thieu

---
+++ Buoi4/bai4/bai4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace phanloaitamgiac
+{
+    class tamgiac
+    {
+        static void Main(string[] args)
+        {
+            double a,b,c;
+            Console.WriteLine("Moi ban nhap ba so thuc a,b,c: ");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            c = double.Parse(Console.ReadLine());
+            String kq = " ";
+            if (a == b && b == c)
+            {
+                kq += "deu";
+            }
+            else if ((a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b * b) && (a == b || b == c || c == a))
+            {
+                kq += "vuong can";
+            }
+            else if (a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b * b)
+            {
+                kq += "vuong";
+            }
+            else if (a == b || b == c || c == a)
+            {
+                kq += "can";
+            }
+            else
+            {
+                kq += "thuong";
+            }
+            Console.WriteLine($"Tam giac tao thanh la tam giac{kq}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Họ và Tên: Nguyễn Xuân Hậu Thành
 Môn học: Lập trình ngôn ngữ CSharp
 Timeline:
 14/09/2026: Hoàn thành bài tập buổi 4
+17/09/2026: Hoàn thành bài tập buổi 5 và 6
